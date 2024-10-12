0b066ac0b2d0c23ed906690f73072c68649825cd
nethaji <nethunethz@gmail.com>
2024-10-12 15:41:33 +0530

    up

---
+++ terraform/var.tf
@@ -0,0 +1,32 @@
+variable "x" {
+    default = 100
+}
+
+variable "env" {}
+
+variable "x_list" {
+    default = [0,1,2,3]
+}
+
+variable "z_map" {
+    default = {
+        x=10
+        y=20
+    }
+}
+
+output "x_1" {
+    value = var.x
+}
+
+output "list" {
+    value = var.x_list[1]
+}
+
+output "map" {
+    value = var.z_map[x]
+}
+
+output "env" {
+    value = environment name - ${var.env}
+}
